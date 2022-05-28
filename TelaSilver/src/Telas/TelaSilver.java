adc605318f85e0c9adeff8a0b518c4bce7d7cb88
Augusto Torres Santos <77200265+MrZeroLeft@users.noreply.github.com>
2022-05-28 15:24:43 -0300

    Add files via upload

--- TelaSilver/src/Telas/TelaSilver.java
+++ TelaSilver/src/Telas/TelaSilver.java
@@ -11,6 +11,9 @@ import javax.swing.JButton;
 import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
 import javax.swing.JCheckBox;
+import java.awt.Checkbox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaSilver extends JFrame {
 
@@ -31,7 +34,7 @@ public class TelaSilver extends JFrame {
 
 	public TelaSilver() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 600, 300);
+		setBounds(100, 100, 600, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -42,47 +45,82 @@ public class TelaSilver extends JFrame {
 		JComboBox comboBox = new JComboBox(tiposArquivo);
 		comboBox.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent e) {
-				if(comboBox.getSelectedItem() == "PDF") {
+				while(comboBox.getSelectedItem() == "PDF") {
 					JCheckBox chckbxNewCheckBox = new JCheckBox(".pdf");
 					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+					break;
 				}
 				
-				if(comboBox.getSelectedItem() == "Video") {
+				while(comboBox.getSelectedItem() == "Video") {
 					JCheckBox chckbxNewCheckBox = new JCheckBox(".avi");
 					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+					break;
 				}
 				
-				if(comboBox.getSelectedItem() == "Audio") {
+				while(comboBox.getSelectedItem() == "Audio") {
 					JCheckBox chckbxNewCheckBox = new JCheckBox(".mp3");
 					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+					break;
 				}
 				
-				if(comboBox.getSelectedItem() == "PNG") {
+				while(comboBox.getSelectedItem() == "PNG") {
 					JCheckBox chckbxNewCheckBox = new JCheckBox(".png");
 					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+					break;
 				}
 				
-				if(comboBox.getSelectedItem() == "Word") {
+				while(comboBox.getSelectedItem() == "Word") {
 					JCheckBox chckbxNewCheckBox = new JCheckBox(".docx");
 					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+					break;
 				}
 				
-				if(comboBox.getSelectedItem() == "Excel") {
+				while(comboBox.getSelectedItem() == "Excel") {
 					JCheckBox chckbxNewCheckBox = new JCheckBox(".xlsx");
 					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+					break;
 				}
 				
-				if(comboBox.getSelectedItem() == "Power Point") {
+				while(comboBox.getSelectedItem() == "Power Point") {
 					JCheckBox chckbxNewCheckBox = new JCheckBox(".ppt");
 					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+					break;
 				}
+				
+				/*if(comboBox.getSelectedItem() == "PDF") {
+					JCheckBox chckbxNewCheckBox = new JCheckBox(".pdf");
+					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+				} else if(comboBox.getSelectedItem() == "Video") {
+					JCheckBox chckbxNewCheckBox = new JCheckBox(".avi");
+					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+				} else if(comboBox.getSelectedItem() == "Audio") {
+					JCheckBox chckbxNewCheckBox = new JCheckBox(".mp3");
+					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+				} else if(comboBox.getSelectedItem() == "PNG") {
+					JCheckBox chckbxNewCheckBox = new JCheckBox(".png");
+					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+				} else if(comboBox.getSelectedItem() == "Word") {
+					JCheckBox chckbxNewCheckBox = new JCheckBox(".docx");
+					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+				} else if(comboBox.getSelectedItem() == "Excel") {
+					JCheckBox chckbxNewCheckBox = new JCheckBox(".xlsx");
+					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+				} else if(comboBox.getSelectedItem() == "Power Point") {
+					JCheckBox chckbxNewCheckBox = new JCheckBox(".ppt");
+					contentPane.add(chckbxNewCheckBox, BorderLayout.WEST);
+				}*/
 			}
 		});
 		
+		comboBox.setSelectedIndex(0);
+		
 		contentPane.add(comboBox, BorderLayout.NORTH);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Proxima Etapa");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		contentPane.add(btnNewButton, BorderLayout.SOUTH);
 	}
-
 }
